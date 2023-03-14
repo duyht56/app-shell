2f4b98c21875b4cf038c9142a7bae333b38ff9e9
duy.hoang4 <duy.hoang4@niteco.se>
2023-03-14 17:45:25 +0700

    .

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const regexEqual = (x, y) =>
+  x instanceof RegExp &&
+  y instanceof RegExp &&
+  x.source === y.source &&
+  x.global === y.global &&
+  x.ignoreCase === y.ignoreCase &&
+  x.multiline === y.multiline;
 module.exports = withBundleAnalyzer({
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -38,7 +45,7 @@ module.exports = withBundleAnalyzer({
           "./mainNav": "./components/Navigation/MainNav.js",
         },
         remotes: {
-          PDP: `PDP@https://app-pdp-duyhoang-avivetech.vercel.app/_next/static/${location}/remoteEntry.js`,
+          PDP: `PDP@https://app-pdp.vercel.app/_next/static/${location}/remoteEntry.js`,
           Support:
             "Support@https://gray-hill-0a779f603.1.azurestaticapps.net/_next/static/chunks/remoteEntry.js",
         },
@@ -47,6 +54,29 @@ module.exports = withBundleAnalyzer({
           exposePages: true,
         },
       })
+    );
+
+    const sassRules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.find(
+        (rule) =>
+          rule.sideEffects === false &&
+          regexEqual(rule.test, /\.module\.(scss|sass)$/)
+      );
+
+    sassRules.use = sassRules.use.map((rule) =>
+      rule.loader.includes("css-loader/dist")
+        ? {
+            ...rule,
+            options: {
+              ...rule.options,
+              modules: {
+                ...rule.modules,
+                localIdentName: "aa[hash:base64:5]",
+              },
+            },
+          }
+        : rule
     );
 
     return config;
